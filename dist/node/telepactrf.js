ec1c37e071b59fed6936a6912fbcc539e5f501bb
mmast <mmast@mmt.local>
2018-06-15 10:31:49 +0200

    avancement

--- dist/node/telepactrf.js
+++ dist/node/telepactrf.js
@@ -6,24 +6,20 @@
 
 'use strict';
 
+// La propriété est à ajouter avec une valeur
+var addRuleType = 'add';
+
 // La propriété est à supprimer
 var removeRuleType = 'remove';
+
+// La valeur de la propriété est à mofifier
+var valueRuleType = 'value';
 
 // La propriété est à renomer
 var renameRuleType = 'rename';
 
-// La propriété est à ajouter
-var addRuleType = 'add';
-
-
-if (typeof String.prototype.trim !== 'function') {
-  String.prototype.trim = function() {
-    return this.replace(/^\s+|\s+$/g, '');
-  }
-}
-
 function cloneGeojson(geojson) {
-  if (geojson === null || typeof(obj) !== 'object') {
+  if (geojson === null || typeof (obj) !== 'object') {
     return geojson;
   }
   var newGeojson = {};
@@ -47,34 +43,34 @@ function telepactrf(geojson, rules) {
   }
 }
 
-function telepactrfTransformer() {}
+function telepactrfTransformer() { }
 
-telepactrfTransformer.prototype.setGeojson = function(geojson) {
+telepactrfTransformer.prototype.setGeojson = function (geojson) {
   this.geojson = geojson;
 }
 
-telepactrfTransformer.prototype.setRules = function(rules) {
+telepactrfTransformer.prototype.setRules = function (rules) {
   this.rules = rules;
 }
 
-telepactrfTransformer.prototype.transform = function() {
+telepactrfTransformer.prototype.transform = function () {
   this.geojson = cloneGeojson(this.geojson);
   if (this.rules && Object.prototype.toString.call(this.rules) === '[object Array]') {
     this.transformGeojson(this.geojson);
   }
 }
 
-telepactrfTransformer.prototype.transformGeojson = function(geojson) {
-  if (geojson) {
-    if (geojson.type === 'Feature') {
-      if (geojson.properties) {
-        this.transformProperties(geojson.properties)
+telepactrfTransformer.prototype.transformGeojson = function (subgeojson) {
+  if (subgeojson) {
+    if (subgeojson.type === 'Feature') {
+      if (subgeojson.properties) {
+        this.transformProperties(subgeojson.properties)
       }
-    } else if (geojson.type === 'FeatureCollection') {
-      if (geojson.features && Object.prototype.toString.call(geojson.features) === '[object Array]') {
+    } else if (subgeojson.type === 'FeatureCollection') {
+      if (subgeojson.features && Object.prototype.toString.call(subgeojson.features) === '[object Array]') {
         var i = 0;
-        while (i < this.geojson.features) {
-          this.transformGeojson(geojson.features[i])
+        while (i < subgeojson.features.length) {
+          this.transformGeojson(subgeojson.features[i])
           i++;
         }
       }
@@ -82,14 +78,54 @@ telepactrfTransformer.prototype.transformGeojson = function(geojson) {
   }
 }
 
-telepactrfTransformer.prototype.transformProperties = function(properties) {
-    /*
-    var i = 0;
-    while (i < this.rules.length) {
-      var rule = this.rules[i];
-      // Todo: traitement de la règle de transformation
-      i++;
-    }*/
+telepactrfTransformer.prototype.transformProperties = function (properties) {
+  if (!properties) {
+    return;
+  }
+  var i = 0;
+  while (i < this.rules.length) {
+    var rule = this.rules[i];
+    if (rule && rule.type && rule.property) {
+      // traitement de la règle de transformation "add"
+      if (rule.type === addRuleType) {
+        if (properties[rule.property] === undefined) {
+          properties[rule.property] = rule.value;
+        } else {
+          console.error('rule "' + rule.type + '"is not correctly defined', rule);
+        }
+      }
+      // traitement de la règle de transformation "remove"
+      else if (rule.type === removeRuleType) {
+        if (properties[rule.property] !== undefined) {
+          delete properties[rule.property];
+        } else {
+          console.error('rule "' + rule.type + '"is not correctly defined', rule);
+        }
+      }
+      // traitement de la règle de transformation "value"
+      else if (rule.type === valueRuleType) {
+        if (properties[rule.property] !== undefined) {
+          properties[rule.property] = rule.value;
+        } else {
+          console.error('rule "' + rule.type + '"is not correctly defined', rule);
+        }
+      }
+      // traitement de la règle de transformation "rename"
+      else if (rule.type === renameRuleType) {
+        if (properties[rule.property] !== undefined && properties[rule.name] === undefined) {
+          properties[rule.name] = properties[rule.property];
+          delete properties[rule.property];
+        } else {
+          console.error('rule "' + rule.type + '"is not correctly defined', rule);
+        }
+      } else {
+        console.error('rule type "' + rule.type + '"is unknown', rule);
+      }
+    } else {
+      console.error('rule is not correctly defined', rule);
+    }
+    i++;
+  }
 }
 
 module.exports = telepactrf;
--- test/load.js
+++ test/load.js
@@ -6,12 +6,75 @@ function read(name) {
   return JSON.parse(fs.readFileSync('test/data/' + name + '.geojson'));
 }
 
-describe('Load', function() {
-  it('telepac_1', function() {
-    var geojson2018 = read('telepac_1_2018');
-    var geojson = telepactrf(geojson2018, [
-
+describe('Load', function () {
+  it('test', function () {
+    var geojsonTest = read('geojson_test');
+    var geojsonResult = telepactrf(geojsonTest, [
+      {
+        "type": "add",
+        "property": "COMMERC",
+        "value": "0"
+      }, {
+        "type": "add",
+        "property": "AIDEBIO",
+        "value": ""
+      }, {
+        "type": "add",
+        "property": "MAEC1_CODE",
+        "value": ""
+      }, {
+        "type": "add",
+        "property": "MAEC1CIBLE",
+        "value": "0"
+      }, {
+        "type": "add",
+        "property": "MAEC2_CODE",
+        "value": ""
+      }, {
+        "type": "add",
+        "property": "MAEC2CIBLE",
+        "value": "0"
+      }, {
+        "type": "add",
+        "property": "MAEC3_CODE",
+        "value": ""
+      }, {
+        "type": "add",
+        "property": "MAEC3CIBLE",
+        "value": "0"
+      }, {
+        "type": "remove",
+        "property": "SURF"
+      },
+      {
+        "type": "remove",
+        "property": "RECONV_PP"
+      },
+      {
+        "type": "remove",
+        "property": "CIBLE_SHP"
+      },
+      {
+        "type": "remove",
+        "property": "MAEC_PRV"
+      },
+      {
+        "type": "value",
+        "property": "NUMERO_PA",
+        "value": "0"
+      },
+      {
+        "type": "rename",
+        "property": "NUMERO_I",
+        "name": "NUMERO_SI"
+      },
+      {
+        "type": "rename",
+        "property": "NUMERO_P",
+        "name": "NUMERO"
+      }
     ]);
-    console.log(geojson);
+    var geojsonExpected = read('geojson_test_expected');
+    assert.deepEqual(geojsonResult, geojsonExpected);
   });
 });
